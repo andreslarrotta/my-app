323648d2c198c0fe1b82a88c9b609c1b51abc202
Andres Felipe Larrotta Pino <alarrotta@chefcompany.co>
2021-10-18 17:28:40 -0500

    header terminado mobile y desktop

--- src/App/index.js
+++ src/App/index.js
@@ -1,24 +1,26 @@
-import { useState } from 'react';
+/* import { useState, useEffect } from 'react'; */
 import './styles.scss';
 import Header from '../Header/index';
 
 const App = () => {
-    const [titulo, setTitulo] = useState('Antes del SteTime')
-    const buttonSend = document.querySelector('.button_send')
-
-    buttonSend.addEventListener('click', () => {
-        let headerContainer = document.querySelector('.Header_container')
-        headerContainer.setAttribute('style', 'background:red')
-        console.log('CONSOLE LOG');
-    })
-
-    setTimeout(() => {
-        setTitulo('despues del SteTime')
-    }, 3000)
-
+    /*  const [titulo, setTitulo] = useState('Antes del SteTime')
+     useEffect(() => {
+         const buttonSend = document.querySelector('.button_send')
+ 
+         buttonSend.addEventListener('click', () => {
+             let headerContainer = document.querySelector('.Header_container')
+             headerContainer.setAttribute('style', 'background:red')
+             console.log('CONSOLE LOG');
+         })
+     })
+ 
+     setTimeout(() => {
+         setTitulo('despues del SteTime')
+     }, 3000)
+  */
     return (
         <>
-            <Header texto={titulo} />
+            <Header />
             <button type="button" className="button_send">Click Me!</button>
         </>
     );
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,18 +1,48 @@
 import './styles.scss';
+import logo from '../assets/andres_larrotta_logo.png';
 
-const Header = (props) => {
+const Header = () => {
     return (
-        <div className="Header">
-            <div className="Header_container">
-                <h1>{props.texto}</h1>
-                <div className="Header_column">
-                    Hola soy un div # 1
-                </div>
-                <div className="Header_column">
-                    Hola soy un div # 2
+        <header>
+            <div className="header">
+                <div className="header_container">
+                    <div className="header_column">
+                        <img src={logo} alt="Andres Felipe Larrotta Pino Logo" />
+                    </div>
+                    <div className="header_column">
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="/about-me">
+                                        About me
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/about-me">
+                                        Skills
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/about-me">
+                                        Experience
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/about-me">
+                                        Portfolio
+                                    </a>
+                                </li>
+                                <li className="download">
+                                    <a href="/about-me">
+                                        Download CV
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
             </div>
-        </div>
+        </header>
     );
 }
 
